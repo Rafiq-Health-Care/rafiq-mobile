8c66f8cd41cdb58fbe214920cda11c98c7c8d00e
Wael Lasheen <lasheenwael9@gmail.com>
2025-10-10 17:25:11 +0300

    ✨ feat(router): implement app routing and initial screens - add app_router for navigation management - define router_strings for route constants - create placeholder screens: landing, login, sign up

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rafiq/core/router/router_strings.dart';
+import 'package:rafiq/features/auth/presentation/screens/login_screen.dart';
+import 'package:rafiq/features/auth/presentation/screens/signup_screen.dart';
+import 'package:rafiq/features/landing/presentation/screens/landing_screen.dart';
+
+class AppRouter {
+  AppRouter();
+
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RouterStrings.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      
+      case RouterStrings.signup:
+        return MaterialPageRoute(builder: (_) => const SignupScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) => const LandingScreen());
+    }
+  }
+}
---
+++ lib/core/router/router_strings.dart
@@ -0,0 +1,5 @@
+abstract class RouterStrings {
+  static const String landing = "/";
+  static const String login = "/login";
+  static const String signup = "/signup";
+}
