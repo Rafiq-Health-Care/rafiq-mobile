a8183551c949fbf80430ac1c0c8c083cf1f352b1
Wael Lasheen <lasheenwael9@gmail.com>
2025-10-12 15:47:36 +0300

    ✨ feat(assets): add new images and update logo paths
    - add new images (img_1.jpg, img_2.jpg, img_3.jpg, logo_1.png, logo_2.png)
    - update image paths in ImageUrl class

--- lib/core/utils/image_url.dart
+++ lib/core/utils/image_url.dart
@@ -3,7 +3,11 @@ class ImageUrl {
   factory ImageUrl() => _instance;
   ImageUrl._();
 
-  final String splashLogo = 'assets/images/logo.png';
+  final String splashLogo = 'assets/images/logo_1.png';
+  final String logo = 'assets/images/logo_2.png';
   final String googleSvg = 'assets/icons/google.svg';
   final String facebookSvg = 'assets/icons/facebook.svg';
+  final String img1 = 'assets/images/img_1.jpg';
+  final String img2 = 'assets/images/img_2.jpg';
+  final String img3 = 'assets/images/img_3.jpg';
 }
