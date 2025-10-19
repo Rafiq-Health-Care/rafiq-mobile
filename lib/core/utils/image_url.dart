66d8e042e0219df357d772e158dc194b95d237e9
Wael Lasheen <lasheenwael9@gmail.com>
2025-10-19 14:05:32 +0300

    ✨ feat(assets): add doctor and patient icons
    - add new doctor and patient icons to the assets
    - update ImageUrl class to include the new icons paths

--- lib/core/utils/image_url.dart
+++ lib/core/utils/image_url.dart
@@ -12,4 +12,6 @@ class ImageUrl {
   final String img3 = 'assets/images/img_3.jpg';
   final String doctorLogo = 'assets/images/doctor_logo.svg';
   final String patientLogo = 'assets/images/patient_logo.svg';
+  final String doctorIcon = 'assets/icons/doctor_icon.png';
+  final String patientIcon = 'assets/icons/patient_icon.png';
 }
