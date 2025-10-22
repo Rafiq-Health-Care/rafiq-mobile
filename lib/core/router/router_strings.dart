b79ff60ec192d399381056833c0b60a0410da4fe
Wael Lasheen <lasheenwael9@gmail.com>
2025-10-22 19:28:34 +0300

    ✨ feat(router): add new routes for sign up flow
    - add routes for patient and doctor sign up steps
    - add route for doctor id upload screen
    - add route for otp screen

--- lib/core/router/router_strings.dart
+++ lib/core/router/router_strings.dart
@@ -3,6 +3,10 @@ abstract class RouterStrings {
   static const String onBoarding = "/on_boarding";
   static const String login = "/login";
   static const String selectUserType = "/select_user_type";
-  static const String signUpPatient = "/sign_up_patient";
-  static const String signUpDoctor = "/sign_up_doctor";
+  static const String signUpPatientStepI = "/sign_up_patient_step_i";
+  static const String signUpPatientStepII = "/sign_up_patient_step_ii";
+  static const String doctorIdUploadScreen = "/doctor_id_upload_screen";
+  static const String signUpDoctorStepI = "/sign_up_doctor_step_i";
+  static const String signUpDoctorStepII = "/sign_up_doctor_step_ii";
+  static const String otp = "/otp";
 }
