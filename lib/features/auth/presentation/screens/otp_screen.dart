4b16e07657f521f5d5c3de7c8a3d88085c4b857f
Wael Lasheen <lasheenwael9@gmail.com>
2025-10-30 10:08:13 +0300

    ✨ feat(auth): add support for forget password flow in OTP screen
    - add isForgetPassword parameter to OtpScreen to differentiate between signup and forget password flows
    - implement onSubmit method to call appropriate cubit method based on isForgetPassword value
    - add MultiBlocListener to listen to both AuthCubit and ForgetPasswordCubit states
    - navigate to change password screen on ForgetPasswordOtpVerified state
    - show snack bar message on AuthFailure or ForgetPasswordError state
    - disable submit button while loading

--- lib/features/auth/presentation/screens/otp_screen.dart
+++ lib/features/auth/presentation/screens/otp_screen.dart
@@ -1,15 +1,17 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinput/pinput.dart';
 import 'package:rafiq/core/functions/snack_bar_message.dart';
+import 'package:rafiq/core/router/router_strings.dart';
 import 'package:rafiq/core/theme/app_theme.dart';
 import 'package:rafiq/features/auth/controllers/auth_cubit/auth_cubit.dart';
+import 'package:rafiq/features/auth/controllers/forget_password_cubit/forget_password_cubit.dart';
 import 'package:rafiq/features/auth/presentation/widgets/text_with_action_link.dart';
 import 'package:rafiq/core/widgets/custom_elevated_button.dart';
 
 class OtpScreen extends StatefulWidget {
-  const OtpScreen({super.key});
+  final bool isForgetPassword;
+  const OtpScreen({super.key, required this.isForgetPassword});
 
   @override
   State<OtpScreen> createState() => _OtpScreenState();
@@ -22,6 +24,16 @@ class _OtpScreenState extends State<OtpScreen> {
   void dispose() {
     super.dispose();
     _pinController.dispose();
+  }
+
+  void onSubmit() {
+    final code = _pinController.text;
+
+    if (code.length == 6) {
+      widget.isForgetPassword
+          ? ForgetPasswordCubit.get(context).userVerify(code)
+          : AuthCubit.get(context).userVerification(code);
+    }
   }
 
   @override
@@ -61,41 +73,63 @@ class _OtpScreenState extends State<OtpScreen> {
                 ),
               ),
               const Spacer(),
-              TextWithActionLink(
-                staticText: "Didn't receive a code? ",
-                linkText: 'Resend code',
-                onTap: () {
-                  authCubit.sendNewOtp();
-                },
-              ),
+              if (!widget.isForgetPassword)
+                TextWithActionLink(
+                  staticText: "Didn't receive a code? ",
+                  linkText: 'Resend code',
+                  onTap: () {
+                    authCubit.sendNewOtp();
+                  },
+                ),
               const SizedBox(height: 30),
-              BlocConsumer<AuthCubit, AuthState>(
-                listener: (context, state) {
-                  if (state is UserVerificationSuccess) {
-                    // Navigator.of(context).pushNamed(RouterStrings.otp);
-                    if (kDebugMode) {
-                      print('Success');
-                    }
-                  } else if (state is AuthFailure) {
-                    snackBarMessage(context, state.message);
-                  }
-                },
-                builder: (context, state) {
-                  bool isLoading = state is AuthLoading;
-                  return CustomElevatedButton(
-                    onPressed: () {
-                      final code = _pinController.text;
-                      if (code.length == 6 && !isLoading) {
-                        authCubit.userVerification(code);
+              MultiBlocListener(
+                listeners: [
+                  BlocListener<AuthCubit, AuthState>(
+                    listener: (context, state) {
+                      if (state is UserVerificationSuccess) {
+                        // Navigator.of(context).pushNamed(RouterStrings.otp);
+                      } else if (state is AuthFailure) {
+                        snackBarMessage(context, state.message);
                       }
                     },
-                    backgroundColor: appTheme.deepDarkBlueColor,
-                    foregroundColor: appTheme.surfaceColor,
-                    child: isLoading
-                        ? const CircularProgressIndicator()
-                        : Text('Submit', style: appTheme.buttonLabelTextStyle),
-                  );
-                },
+                  ),
+                  BlocListener<ForgetPasswordCubit, ForgetPasswordState>(
+                    listener: (context, state) {
+                      if (state is ForgetPasswordOtpVerified) {
+                        Navigator.of(
+                          context,
+                        ).pushNamed(RouterStrings.changePassword);
+                      } else if (state is ForgetPasswordError) {
+                        snackBarMessage(context, state.message);
+                      }
+                    },
+                  ),
+                ],
+                child: BlocBuilder<AuthCubit, AuthState>(
+                  builder: (context, authState) =>
+                      BlocBuilder<ForgetPasswordCubit, ForgetPasswordState>(
+                        builder: (context, forgetPasswordState) {
+                          bool isLoading =
+                              authState is AuthLoading ||
+                              forgetPasswordState is ForgetPasswordLoading;
+                          return CustomElevatedButton(
+                            onPressed: () {
+                              if (!isLoading) {
+                                onSubmit();
+                              }
+                            },
+                            backgroundColor: appTheme.deepDarkBlueColor,
+                            foregroundColor: appTheme.surfaceColor,
+                            child: isLoading
+                                ? const CircularProgressIndicator()
+                                : Text(
+                                    'Submit',
+                                    style: appTheme.buttonLabelTextStyle,
+                                  ),
+                          );
+                        },
+                      ),
+                ),
               ),
             ],
           ),
