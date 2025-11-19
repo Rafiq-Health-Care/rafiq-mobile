2469b94a3f280e8b70e486dccb59731344f40d54
Wael Lasheen <lasheenwael9@gmail.com>
2025-11-19 07:34:08 +0200

    ✨ feat(lab_test): implement lab test repository
    - create lab test repository with methods for fetching, creating, deleting, and updating lab tests
    - add methods for getting all lab tests, details, uploading, saving and updating results

---
+++ lib/features/lab_test/data/repository/lab_test_repository.dart
@@ -0,0 +1,47 @@
+import 'package:rafiq/features/lab_test/data/models/lab_test_get_all_request.dart';
+import 'package:rafiq/features/lab_test/data/models/lab_test_get_all_response.dart';
+import 'package:rafiq/features/lab_test/data/models/lab_test_get_details_response.dart';
+import 'package:rafiq/features/lab_test/data/models/lab_test_results_model.dart';
+import 'package:rafiq/features/lab_test/data/models/lab_test_upload_request.dart';
+import 'package:rafiq/features/lab_test/data/models/lab_test_upload_response.dart';
+import 'package:rafiq/features/lab_test/data/networking/lab_test_service.dart';
+
+class LabTestRepository {
+  final LabTestService labTestService;
+  LabTestRepository({required this.labTestService});
+
+  Future<LabTestGetAllResponse> getAllLabTests(
+    LabTestGetAllRequest request,
+  ) async {
+    final rowData = await labTestService.getAllLabTests(request);
+    return LabTestGetAllResponse.fromJson(rowData);
+  }
+
+  Future<void> deleteAllTestLabs() async {
+    await labTestService.deleteAllTestLabs();
+  }
+
+  Future<LabTestGetDetailsResponse> getTestLabDetails(String testId) async {
+    final rowData = await labTestService.getLabTestDetails(testId);
+    return LabTestGetDetailsResponse.fromJson(rowData);
+  }
+
+  Future<void> deleteTestLab(String testId) async {
+    await labTestService.deleteTestLabDetails(testId);
+  }
+
+  Future<LabTestUploadResponse> uploadTestLab(
+    LabTestUploadRequest request,
+  ) async {
+    final rowData = await labTestService.uploadTestLab(request);
+    return LabTestUploadResponse.fromJson(rowData);
+  }
+
+  Future<void> saveLabTestResults(LabTestResultsModel request) async {
+    await labTestService.saveLabTestResults(request);
+  }
+
+  Future<void> updateLabTestResults(LabTestResultsModel request) async {
+    await labTestService.updateLabTestResults(request);
+  }
+}
