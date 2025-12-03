d71cdc4a1af9ceeee4b728c4329acda3cbd298b8
Wael Lasheen <lasheenwael9@gmail.com>
2025-12-03 21:06:46 +0200

    ✨ feat(image): replace cloud computing icon with png
    - replace cloud-computing.svg to cloud-computing.png
    - the svg file is not rendered correctly.

--- lib/core/utils/image_url.dart
+++ lib/core/utils/image_url.dart
@@ -12,6 +12,6 @@ class ImageUrl {
   final String img3 = 'assets/images/img_3.jpg';
   final String doctorIcon = 'assets/icons/doctor_icon.png';
   final String patientIcon = 'assets/icons/patient_icon.png';
-  final String cloudComputing = 'assets/icons/cloud-computing.svg';
+  final String cloudComputing = 'assets/icons/cloud-computing.png';
   final String loading = 'assets/icons/loading.svg';
 }
