18a70f47de67a5ee1821f9a94e03ec489ac5cc54
Wael Lasheen <lasheenwael9@gmail.com>
2025-12-04 18:28:12 +0200

    ✨ feat(lab_test): implement download/delete buttons widget
    - add a new widget `DownloadDeleteButtons` to handle downloading and deleting lab test files
    - include custom icon button for download functionality
    - use bloc listener for handling success and error states after delete operation
    - display snack bar message upon error during delete operation

---
+++ lib/features/lab_test/presentation/widgets/download_delete_buttons.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rafiq/core/functions/snack_bar_message.dart';
+import 'package:rafiq/core/widgets/custom_icon_button.dart';
+import 'package:rafiq/features/lab_test/controller/lab_test_cubit/lab_test_cubit.dart';
+
+class DownloadDeleteButtons extends StatelessWidget {
+  final VoidCallback onClickDownload;
+  final VoidCallback onClickDelete;
+
+  const DownloadDeleteButtons({
+    super.key,
+    required this.onClickDownload,
+    required this.onClickDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 16,
+      children: [
+        Expanded(
+          child: CustomIconButton(
+            onPress: onClickDownload,
+            label: 'Download File',
+            icon: Icons.download,
+          ),
+        ),
+        BlocListener<LabTestCubit, LabTestState>(
+          listener: (context, state) {
+            if (state is LabTestSuccess) {
+              Navigator.pop(context);
+            } else if (state is LabTestError) {
+              snackBarMessage(context, state.message);
+            }
+          },
+          child: IconButton(
+            icon: const Icon(Icons.delete, color: Colors.red, size: 30),
+            onPressed: onClickDelete,
+          ),
+        ),
+      ],
+    );
+  }
+}
