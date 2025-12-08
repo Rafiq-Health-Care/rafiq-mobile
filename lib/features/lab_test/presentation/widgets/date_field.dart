94aba1458d7411707f2941fb6574550b613e6171
Wael Lasheen <lasheenwael9@gmail.com>
2025-12-08 17:40:45 +0200

    Add DateField widget with ValueNotifier binding and themed date picker integration

---
+++ lib/features/lab_test/presentation/widgets/date_field.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:rafiq/core/theme/app_theme.dart';
+import 'package:rafiq/features/lab_test/data/services/pick_date_service.dart';
+
+class DateField extends StatelessWidget {
+  final ValueNotifier<DateTime> dateNotifier;
+  const DateField({super.key, required this.dateNotifier});
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme appTheme = Theme.of(context).extension<AppTheme>()!;
+    return ValueListenableBuilder<DateTime>(
+      valueListenable: dateNotifier,
+      builder: (context, date, _) {
+        return InkWell(
+          onTap: () async {
+            final picked = await PickDateService().pickDate(context, date);
+            if (picked != null) {
+              dateNotifier.value = picked;
+            }
+          },
+          child: InputDecorator(
+            decoration: InputDecoration(
+              labelText: 'Test Date',
+              prefixIcon: const Icon(Icons.calendar_today_outlined),
+              border: appTheme.textFieldBorder,
+              enabledBorder: appTheme.textFieldBorder,
+              filled: true,
+              fillColor: Colors.grey[50],
+            ),
+            child: Text(
+              DateFormat('MM/dd/yyyy').format(date),
+              style: const TextStyle(fontSize: 16),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
