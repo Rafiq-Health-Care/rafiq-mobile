674a6048e51013f05468f867068e68ce8a8e9b1f
Wael Lasheen <lasheenwael9@gmail.com>
2025-12-08 17:40:14 +0200

    Add BottomActions widget with cancel flow and loading-aware save button using LabTestCubit

---
+++ lib/features/lab_test/presentation/widgets/bottom_action.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rafiq/core/theme/app_theme.dart';
+import 'package:rafiq/core/widgets/custom_elevated_button.dart';
+import 'package:rafiq/core/widgets/custom_outlined_button.dart';
+import 'package:rafiq/features/lab_test/controller/lab_test_cubit/lab_test_cubit.dart';
+
+class BottomActions extends StatelessWidget {
+  final VoidCallback onSave;
+  const BottomActions({super.key, required this.onSave});
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme appTheme = Theme.of(context).extension<AppTheme>()!;
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.05),
+            blurRadius: 10,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.all(16),
+      child: SafeArea(
+        child: Row(
+          spacing: 12,
+          children: [
+            Expanded(
+              child: CustomOutlinedButton(
+                onPressed: () => Navigator.pop(context),
+                foregroundColor: appTheme.surfaceColor,
+                borderSideColor: Colors.grey[300]!,
+                child: Text(
+                  'Cancel',
+                  style: appTheme.buttonLabelTextStyle.copyWith(
+                    color: appTheme.deepDarkBlueColor,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: BlocBuilder<LabTestCubit, LabTestState>(
+                builder: (context, state) {
+                  bool isLoading = state is LabTestLoading;
+                  return CustomElevatedButton(
+                    onPressed: () {
+                      if (!isLoading) onSave();
+                    },
+                    backgroundColor: appTheme.deepDarkBlueColor,
+                    foregroundColor: appTheme.surfaceColor,
+                    child: isLoading? const CircularProgressIndicator() : Text(
+                      'Save & Continue',
+                      style: appTheme.buttonLabelTextStyle,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
