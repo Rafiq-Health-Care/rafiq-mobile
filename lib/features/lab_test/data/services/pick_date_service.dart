b6085acce03ee9c30168e3d177efc2b08d605195
Wael Lasheen <lasheenwael9@gmail.com>
2025-12-08 17:39:12 +0200

    Add PickDateService to handle themed date picker with customizable primary color

---
+++ lib/features/lab_test/data/services/pick_date_service.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rafiq/core/theme/app_theme.dart';
+
+class PickDateService {
+  Future<DateTime?> pickDate(BuildContext context, DateTime initialDate) async {
+    final appTheme = Theme.of(context).extension<AppTheme>()!;
+    return await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(primary: appTheme.deepDarkBlueColor),
+          ),
+          child: child!,
+        );
+      },
+    );
+  }
+}
