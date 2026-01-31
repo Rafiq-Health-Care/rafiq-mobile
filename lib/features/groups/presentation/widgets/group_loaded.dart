bafd3ba4d3e0ba80277e819d6afa535427e09644
Wael Lasheen <lasheenwael9@gmail.com>
2026-01-31 13:57:53 +0200

    Adds GroupLoaded widget
    Introduces the GroupLoaded widget to display the list of medicine groups.
    
    This widget handles the display of the group list based on the provided state, including loading, empty, and search result states.
    
    It also includes a search bar for filtering groups and a button to create new groups.

---
+++ lib/features/groups/presentation/widgets/group_loaded.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rafiq/core/router/router_strings.dart';
+import 'package:rafiq/core/theme/app_theme.dart';
+import 'package:rafiq/core/utils/image_url.dart';
+import 'package:rafiq/core/widgets/custom_icon_button.dart';
+import 'package:rafiq/core/widgets/custom_screen_header.dart';
+import 'package:rafiq/core/widgets/custom_search_bar.dart';
+import 'package:rafiq/core/widgets/empty_state_widget.dart';
+import 'package:rafiq/features/groups/controllers/group_cubit/group_cubit.dart';
+import 'package:rafiq/features/groups/presentation/widgets/group_card.dart';
+
+class GroupLoadedWidget extends StatelessWidget {
+  final GroupLoaded state;
+  final TextEditingController searchController;
+
+  const GroupLoadedWidget({
+    super.key,
+    required this.state,
+    required this.searchController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = Theme.of(context).extension<AppTheme>()!;
+    return Column(
+      spacing: 16,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScreenHeader(
+            title: 'Medicine Groups',
+            description: 'View and manage your collection of medicines.',
+            total: state.currentGroups.length,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomSearchBar(
+            searchController: searchController,
+            onSearch: GroupCubit.of(context).search,
+            hintText: 'Search for a group...',
+          ),
+        ),
+        if (state.allGroups.isEmpty)
+          Expanded(
+            child: EmptyStateWidget(
+              icon: SvgPicture.asset(ImageUrl().empty),
+              title: 'No Groups Added Yet',
+              description: 'Get started by creating your first medicine group.',
+            ),
+          )
+        else if (state.currentGroups.isEmpty)
+          Expanded(
+            child: EmptyStateWidget(
+              icon: SvgPicture.asset(ImageUrl().noSearchResult),
+              title: 'No Groups found',
+              description: 'Try different keywords.',
+            ),
+          )
+        else
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              itemCount: state.currentGroups.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: GroupCard(group: state.currentGroups[index]),
+                );
+              },
+            ),
+          ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8, left: 16, right: 16),
+          child: CustomIconButton(
+            onPress: () {
+              Navigator.pushNamed(context, RouterStrings.upsertGroup);
+            },
+            label: 'Create New Group',
+            icon: Icons.add,
+            fontSize: 18,
+            labelColor: Colors.white,
+            borderRadius: 16,
+            backgroundColor: appTheme.cyanColor400,
+          ),
+        ),
+      ],
+    );
+  }
+}
