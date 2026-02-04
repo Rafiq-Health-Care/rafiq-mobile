8c2baa91309d368b2fc5cfbc013a2c1dfedf2c1b
Wael Lasheen <lasheenwael9@gmail.com>
2026-02-04 13:48:43 +0200

    Improves OTP screen validation and flow
    Refactors the OTP screen to use a form for validation, ensuring that the entered OTP is valid before proceeding.
    
    Removes the dependency on the forget password cubit and simplifies the logic for verifying the OTP, making the code cleaner and more maintainable.

--- lib/features/auth/presentation/screens/otp_screen.dart
+++ lib/features/auth/presentation/screens/otp_screen.dart
@@ -1,27 +1,26 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinput/pinput.dart';
 import 'package:rafiq/core/functions/snack_bar_message.dart';
 import 'package:rafiq/core/router/router_strings.dart';
+import 'package:rafiq/core/services/validation.dart';
 import 'package:rafiq/core/theme/app_theme.dart';
 import 'package:rafiq/core/utils/image_url.dart';
 import 'package:rafiq/core/widgets/custom_app_bar.dart';
 import 'package:rafiq/features/auth/controllers/auth_cubit/auth_cubit.dart';
-import 'package:rafiq/features/auth/controllers/forget_password_cubit/forget_password_cubit.dart';
 import 'package:rafiq/features/auth/presentation/widgets/text_with_action_link.dart';
 import 'package:rafiq/core/widgets/custom_elevated_button.dart';
 
 class OtpScreen extends StatefulWidget {
-  final bool isForgetPassword;
-  const OtpScreen({super.key, required this.isForgetPassword});
+  const OtpScreen({super.key});
 
   @override
   State<OtpScreen> createState() => _OtpScreenState();
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _pinController = TextEditingController();
   bool canResend = false;
   Timer? _timer;
@@ -48,12 +47,8 @@ class _OtpScreenState extends State<OtpScreen> {
   }
 
   void onSubmit() {
-    final code = _pinController.text;
-
-    if (code.length == 6) {
-      widget.isForgetPassword
-          ? ForgetPasswordCubit.get(context).userVerify(code)
-          : AuthCubit.get(context).userVerification(code);
+    if (_formKey.currentState!.validate()) {
+      AuthCubit.get(context).userVerification(_pinController.text);
     }
   }
 
@@ -68,44 +63,46 @@ class _OtpScreenState extends State<OtpScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(ImageUrl().otpLogo, height: size.height * 0.25),
-              const SizedBox(height: 16),
-              Text(
-                'OTP Verification Code',
-                style: appTheme.headingTextStyle.copyWith(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                'Enter the OTP code sent to your email',
-                style: appTheme.bodyTextStyle.copyWith(
-                  color: appTheme.greyColor7,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              Pinput(
-                length: 6,
-                controller: _pinController,
-                focusedPinTheme: PinTheme(
-                  width: 40,
-                  height: 54,
-                  textStyle: appTheme.headingTextStyle.copyWith(fontSize: 24),
-                  decoration: BoxDecoration(
-                    color: appTheme.fieldFillColor,
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Color(0x268AA9D2)),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(ImageUrl().otpLogo, height: size.height * 0.25),
+                const SizedBox(height: 16),
+                Text(
+                  'OTP Verification Code',
+                  style: appTheme.headingTextStyle.copyWith(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              if (!widget.isForgetPassword)
+                const SizedBox(height: 8),
+                Text(
+                  'Enter the OTP code sent to your email',
+                  style: appTheme.bodyTextStyle.copyWith(
+                    color: appTheme.greyColor7,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                Pinput(
+                  length: 6,
+                  controller: _pinController,
+                  validator: Validation.validateOtp,
+                  focusedPinTheme: PinTheme(
+                    width: 40,
+                    height: 54,
+                    textStyle: appTheme.headingTextStyle.copyWith(fontSize: 24),
+                    decoration: BoxDecoration(
+                      color: appTheme.fieldFillColor,
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Color(0x268AA9D2)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
                 TextWithActionLink(
                   staticText: "Didn't receive a code? ",
                   linkText: 'Click here to resend.',
@@ -124,13 +121,14 @@ class _OtpScreenState extends State<OtpScreen> {
                     }
                   },
                 ),
-              SizedBox(height: size.height * 0.25),
-              SizedBox(
-                width: size.width * 0.8,
-                child: customButton(appTheme, context),
-              ),
-              const SizedBox(height: 30),
-            ],
+                SizedBox(height: size.height * 0.25),
+                SizedBox(
+                  width: size.width * 0.8,
+                  child: customButton(appTheme, context),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
           ),
         ),
       ),
@@ -138,55 +136,35 @@ class _OtpScreenState extends State<OtpScreen> {
   }
 
   Widget customButton(final AppTheme appTheme, final BuildContext context) {
-    return MultiBlocListener(
-      listeners: [
-        BlocListener<AuthCubit, AuthState>(
-          listener: (context, state) {
-            if (state is UserVerificationSuccess) {
-              Navigator.of(
-                context,
-              ).pushNamedAndRemoveUntil(RouterStrings.home, (route) => false);
-            } else if (state is AuthFailure) {
-              snackBarMessage(context, state.message);
-            } else if (state is UserVerificationResendSuccess) {
-              resetTimer();
-            }
-          },
-        ),
-        BlocListener<ForgetPasswordCubit, ForgetPasswordState>(
-          listener: (context, state) {
-            if (state is ForgetPasswordOtpVerified) {
-              Navigator.of(context).pushNamed(RouterStrings.changePassword);
-            } else if (state is ForgetPasswordError) {
-              snackBarMessage(context, state.message);
-            }
-          },
-        ),
-      ],
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is UserVerificationSuccess) {
+          Navigator.of(
+            context,
+          ).pushNamedAndRemoveUntil(RouterStrings.home, (route) => false);
+        } else if (state is AuthFailure) {
+          snackBarMessage(context, state.message);
+        } else if (state is UserVerificationResendSuccess) {
+          resetTimer();
+        }
+      },
+      
       child: BlocBuilder<AuthCubit, AuthState>(
-        builder: (context, authState) =>
-            BlocBuilder<ForgetPasswordCubit, ForgetPasswordState>(
-              builder: (context, forgetPasswordState) {
-                bool isLoading =
-                    authState is AuthLoading ||
-                    forgetPasswordState is ForgetPasswordLoading;
-                return CustomElevatedButton(
-                  onPressed: () {
-                    if (!isLoading) {
-                      onSubmit();
-                    }
-                  },
-                  backgroundColor: appTheme.deepDarkBlueColor,
-                  foregroundColor: appTheme.surfaceColor,
-                  child: isLoading
-                      ? const CircularProgressIndicator()
-                      : Text(
-                          'Verify Account',
-                          style: appTheme.buttonLabelTextStyle,
-                        ),
-                );
-              },
-            ),
+        builder: (context, authState) {
+          bool isLoading = authState is AuthLoading;
+          return CustomElevatedButton(
+            onPressed: () {
+              if (!isLoading) {
+                onSubmit();
+              }
+            },
+            backgroundColor: appTheme.deepDarkBlueColor,
+            foregroundColor: appTheme.surfaceColor,
+            child: isLoading
+                ? const CircularProgressIndicator()
+                : Text('Verify Account', style: appTheme.buttonLabelTextStyle),
+          );
+        },
       ),
     );
   }
