be4d9934ba30460dcf78a6fd1172d2a388338376
Wael Lasheen <lasheenwael9@gmail.com>
2026-02-18 12:03:20 +0200

    create UpsertGroupScreen to add and edit group + refactor code to small widgets
    Note: this will update in future after back return colors in HEX

---
+++ lib/features/groups/presentation/screens/upsert_group_screen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:rafiq/core/services/validation.dart';
+import 'package:rafiq/core/utils/extensions/get_app_theme.dart';
+import 'package:rafiq/core/widgets/custom_app_bar.dart';
+import 'package:rafiq/core/widgets/custom_labeled_text_field.dart';
+import 'package:rafiq/core/widgets/custom_screen_header.dart';
+import 'package:rafiq/features/groups/controllers/group_cubit/group_cubit.dart';
+import 'package:rafiq/features/groups/data/models/group_content_model.dart';
+import 'package:rafiq/core/widgets/take_action_or_cancel_button.dart';
+import 'package:rafiq/features/groups/data/models/group_upsert_request.dart';
+import 'package:rafiq/features/groups/presentation/widgets/color_picker.dart';
+
+class UpsertGroupScreen extends StatefulWidget {
+  final GroupContentModel? group;
+  const UpsertGroupScreen({super.key, this.group});
+
+  @override
+  State<UpsertGroupScreen> createState() => _UpsertGroupScreenState();
+}
+
+class _UpsertGroupScreenState extends State<UpsertGroupScreen> {
+  late TextEditingController _nameController;
+  late TextEditingController _descriptionController;
+  final ValueNotifier<Color> _selectedColor = ValueNotifier(Colors.blue);
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final List<Color> _colors = [
+    Colors.grey,
+    Colors.grey.shade300,
+    Colors.blue,
+    Colors.blueAccent,
+    Colors.cyan,
+    Colors.blue.shade400,
+    Colors.teal,
+    Colors.green,
+    Colors.greenAccent,
+    Colors.deepPurple,
+    Colors.deepPurpleAccent,
+    Colors.purple.shade300,
+    Colors.redAccent,
+    Colors.deepOrange,
+    Colors.orange,
+    Colors.amber,
+    Colors.pinkAccent,
+    Colors.brown.shade300,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: widget.group?.name);
+    _descriptionController = TextEditingController(
+      text: widget.group?.description,
+    );
+
+    if (widget.group != null) {
+      _selectedColor.value = Colors.amber;
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    _selectedColor.dispose();
+    super.dispose();
+  }
+
+  void _onSave() {
+    if (_formKey.currentState!.validate()) {
+      final request = GroupUpsertRequest(
+        name: _nameController.text,
+        description: _descriptionController.text,
+        // waiting backend to return the color in HEX
+        color: widget.group != null ? "RED" : "BLUE",
+      );
+
+      if (widget.group == null) {
+        GroupCubit.of(context).addGroup(request);
+      } else {
+        GroupCubit.of(context).updateGroup(widget.group!.id, request);
+      }
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = context.appTheme;
+    final isEditing = widget.group != null;
+
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            spacing: 24,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomScreenHeader(
+                title: isEditing
+                    ? 'Edit Medicine Group'
+                    : 'Create Medicine Group',
+                description:
+                    'Organize your medications into custom groups for easier management.',
+              ),
+              CustomLabeledTextField(
+                label: 'Medicine Group Name',
+                hint: 'e.g Morning Medications',
+                controller: _nameController,
+                validator: (value) =>
+                    Validation.validateNonEmpty(value, 'Medicine Group Name'),
+                textInputAction: TextInputAction.next,
+              ),
+              CustomLabeledTextField(
+                label: 'Description',
+                hint: 'e.g Medications to be taken after breakfast',
+                isOptional: true,
+                controller: _descriptionController,
+                numberOfLines: 4,
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+              ),
+              ColorPicker(
+                selectedColorNotifier: _selectedColor,
+                colors: _colors,
+              ),
+              const SizedBox.shrink(),
+              TakeActionOrCancelButton(
+                action: _onSave,
+                actionText: 'Save & Continue',
+                cancelBackgroundColor: appTheme.softBlueColor,
+                cancelForegroundColor: appTheme.deepDarkBlueColor,
+                cancelTextStyle: appTheme.buttonLabelTextStyle.copyWith(
+                  color: appTheme.deepDarkBlueColor,
+                ),
+              ),
+              const SizedBox.shrink(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
